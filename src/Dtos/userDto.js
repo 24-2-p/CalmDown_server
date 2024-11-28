450ee4917c08eeb1f07a39afbbaa8af8a4193adc
taehun kang <xogns991124@naver.com>
2024-11-29 01:05:05 +0900

    feet: File Structure modify

--- src/Dtos/userDto.js
+++ src/Dtos/userDto.js
@@ -1,11 +1,11 @@
-class UserDto {
-    constructor(id, email, name, mbti, techStack) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-        this.mbti = mbti;
-        this.techStack = techStack;
-    }
-}
-
-module.exports = UserDto;
+// class UserDto {
+//     constructor(id, email, name, mbti, techStack) {
+//         this.id = id;
+//         this.email = email;
+//         this.name = name;
+//         this.mbti = mbti;
+//         this.techStack = techStack;
+//     }
+// }
+//
+// module.exports = UserDto;
--- src/Repositories/userRepository.js
+++ src/Repositories/userRepository.js
@@ -1,11 +1,11 @@
-class UserRepository {
-    async findByEmail(email) {
-        // DB에서 유저 찾기
-    }
-
-    async create(userDto) {
-        // DB에 유저 생성
-    }
-}
-
-module.exports = new UserRepository();
+// class UserRepository {
+//     async findByEmail(email) {
+//         // DB에서 유저 찾기
+//     }
+//
+//     async create(userDto) {
+//         // DB에 유저 생성
+//     }
+// }
+//
+// module.exports = new UserRepository();
